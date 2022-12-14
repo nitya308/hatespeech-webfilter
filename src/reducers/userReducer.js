89748ad15b7738bca276e3a2e615f197600b9cf8
nitya-cs <nitya.agarwala@gmail.com>
2022-12-15 01:55:35 +0530

    added redux reducers

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,26 @@
+import { LOGIN_USER, ADD_WORD } from "../actionTypes/actionTypes";
+
+const initialState = {
+  userID: null,
+  userName: null,
+  words: [],
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_USER:
+      return {
+        ...state,
+        userID: action.payload.userID,
+        userName: action.payload.userName,
+      };
+
+    case ADD_WORD:
+      return {
+        ...state,
+        words: [...state.words, action.payload],
+      };
+    default:
+      return state;
+  }
+};
